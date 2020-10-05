4b4581e76a6ca75948234a08cb84a4edb2ab0769
Takamichi Wada <31792905+wadayamada@users.noreply.github.com>
2020-10-05 22:21:12 +0900

    Update practice1.sql

--- practice1.sql
+++ practice1.sql
@@ -1,6 +1,10 @@
 /*
 EMP(社員)表から、DEPTNO(部署番号)の降順に、DEPTNOとSAL(給料)の平均、部署ごとの人数を表示してください。
 表示するのは、DEPTNO(部署番号)が10以外の部署でお願いします。
+
+以下のリンクの演習問題の改題
+グループ化（GROUP BY）
+http://sql.main.jp/sql09.html
 */
 
 select deptno,avg(sal),count(*)
